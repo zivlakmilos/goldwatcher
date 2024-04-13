4c167dbad40f269983aae01562f5b57239bf02cf
Milos Zivlak <zivlakmilos@gmail.com>
2024-04-13 04:17:15 +0200

    GUI for prices

--- private/gui/mainwindow.go
+++ private/gui/mainwindow.go
@@ -1,17 +1,23 @@
 package gui
 
 import (
+	"fmt"
+	"image/color"
 	"log"
 	"os"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"github.com/zivlakmilos/goldwatcher/private/api"
 )
 
 type MainWindow struct {
-	app      fyne.App
-	win      fyne.Window
-	infoLog  *log.Logger
-	errorLog *log.Logger
+	app            fyne.App
+	win            fyne.Window
+	infoLog        *log.Logger
+	errorLog       *log.Logger
+	priceContainer *fyne.Container
 }
 
 func NewMainWindow(app fyne.App) *MainWindow {
@@ -36,4 +42,44 @@ func (w *MainWindow) Show() {
 }
 
 func (w *MainWindow) setupUI() {
+	openPrice, currentPrice, priceChange := w.getPriceText()
+
+	priceContainer := container.NewGridWithColumns(3,
+		openPrice,
+		currentPrice,
+		priceChange,
+	)
+	w.priceContainer = priceContainer
+
+	finalContent := container.NewVBox(priceContainer)
+	w.win.SetContent(finalContent)
+}
+
+func (w *MainWindow) getPriceText() (*canvas.Text, *canvas.Text, *canvas.Text) {
+	var g api.Gold
+	var open, current, change *canvas.Text
+
+	gold, err := g.GetPrices()
+	if err != nil {
+		gray := color.NRGBA{R: 155, G: 155, B: 155, A: 255}
+		open = canvas.NewText("Open: Unreachable", gray)
+		current = canvas.NewText("Current: Unreachable", gray)
+		change = canvas.NewText("Change: Unreachable", gray)
+	} else {
+		displayColor := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+
+		if gold.Price < gold.PreviousClose {
+			displayColor = color.NRGBA{R: 180, G: 0, B: 0, A: 255}
+		}
+
+		open = canvas.NewText(fmt.Sprintf("Open: $%.4f", gold.PreviousClose), nil)
+		current = canvas.NewText(fmt.Sprintf("Current: $%.4f", gold.PreviousClose), displayColor)
+		change = canvas.NewText(fmt.Sprintf("Change: $%.4f", gold.Change), displayColor)
+	}
+
+	open.Alignment = fyne.TextAlignLeading
+	current.Alignment = fyne.TextAlignCenter
+	change.Alignment = fyne.TextAlignTrailing
+
+	return open, current, change
 }
